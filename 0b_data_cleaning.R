1daf343bff805668ceb2ba4f02818a5626ffbafb
cadaora <clairederksen2025@u.northwestern.edu>
2023-11-25 16:49:51 -0500

    complete data cleaning r file. created new clean rds

--- 0b_data_cleaning.R
+++ 0b_data_cleaning.R
@@ -3,7 +3,7 @@
 
 ## load packages ----
 library(tidyverse)
-
+library(janitor)
 # creating clean dataset -----
 YA_2021 <- read_rds(file="data/NSDUH_2021_YA.rds")
 YA_2020 <- read_rds(file="data/NSDUH_2020_YA.rds")
@@ -30,3 +30,94 @@ YA_19_21 <-YA_19_21 %>%
   mutate(incollege = ifelse(EDUSCHGRD2==9|EDUSCHGRD2==10|EDUSCHGRD2==11,
                             "Enrolled",
                             "Not Enrolled"))
+
+#| label: convert variables
+
+factor_yesno <- function(x) {
+  fct_recode(factor(x, levels = c("1","2")),
+             "Yes"="1",
+             "No"="2") 
+}
+factor_recency <- function(x) {
+  fct_recode(factor(x),
+             "≤ 30 days"="1",
+             "> 30 days, ≤ 12 months"="2",
+             "> 12 months"="3",
+             "Never"="9")
+}
+numeric_frequency <- function(x) {
+  case_when(
+    x == 91 ~ NA,
+    x == 93 ~ 0,
+    .default = x)
+}
+factor_ASDS <- function(x) {
+  fct_recode(factor(x),
+             "None"="1",
+             "Mild"="2",
+             "Moderate"="3",
+             "Severe"="4",
+             "Very Severe"="5")
+}
+
+#convert variables
+YA_19_21 <- YA_19_21 %>%
+  mutate(
+    across(c(STMRSSTDY,AMDEYR,GOVTPROG),factor_yesno),
+    IRCIGRC = fct_recode(factor(IRCIGRC),
+                         "≤ 30 days"="1",
+                         "> 30 days, ≤ 12 months"="2",
+                         "> 12 months, ≤ 3 years"="3",
+                         "> 3 years"="4",
+                         "Never"="9"),
+    across(IRALCRC:IRVAPNICREC,factor_recency),
+    across(IRCIGFM:IRSTMNM30FQ,numeric_frequency),
+    MI_CAT_U = fct_recode(factor(MI_CAT_U),
+                          "None"="0",
+                          "Mild"="1",
+                          "Moderate"="2",
+                          "Serious"="3"),
+    across(ASDSHOM2:ASDSSOC2,factor_ASDS),
+    IRSEX = fct_recode(factor(IRSEX),
+                       "Male"="1",
+                       "Female"="2"),
+    IREDUHIGHST2 = fct_recode(factor(IREDUHIGHST2),
+                              "5th grade or less"="1",
+                              "6th grade"="2",
+                              "7th grade"="3",
+                              "8th grade"="4",
+                              "9th grade"="5",
+                              "10th grade"="6",
+                              "11th or 12th grade, no GED"="7",
+                              "GED"="8",
+                              "Some college"="9",
+                              "Associate's degree"="10",
+                              "College graduate or higher"="11"),
+    CATAG7 = fct_recode(factor(CATAG7),
+                        "18-20"="4",
+                        "21-25"="5"),
+    EDUSCHGRD2 = fct_recode(factor(EDUSCHGRD2),
+                            "5th grade or lower"="1",
+                            "6th grade"="2",
+                            "7th grade"="3",
+                            "8th grade"="4",
+                            "9th grade"="5",
+                            "10th grade"="6",
+                            "11th grade"="7",
+                            "12th grade"="8",
+                            "1st year"="9",
+                            "2nd or 3rd year"="10",
+                            "4th year or higher"="11",
+                            NULL="98",
+                            "Not in school"="99"),
+    INCOME = fct_recode(factor(INCOME),
+                        "Less than $20,000"="1",
+                        "$20,000 - 49,999"="2",
+                        "$50,000 - 74,999"="3",
+                        "$75,000 or more"="4"))
+
+names(clean_names(YA_19_21))
+#write clean rds
+
+YA_19_21 %>%
+  write_rds("data/YA_clean_19_21.rds")
