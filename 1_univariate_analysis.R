b6986e6b71124fc50c5317b317eea67b440c82cb
cadaora <clairederksen2025@u.northwestern.edu>
2023-11-26 13:25:15 -0500

    Completed other univariate analysis and saved to figures

--- 1_univariate_analysis.R
+++ 1_univariate_analysis.R
@@ -8,5 +8,46 @@ library(ggthemes)
 YA_19_21 <-read_rds("data/YA_clean_19_21.rds")
 
 #year count
-YA_19_21 %>%
-  ggplot(aes(year)) + geom_bar(fill="plum4") + theme_fivethirtyeight()
+yearcount <- YA_19_21 %>%
+  ggplot(aes(year)) + 
+  geom_bar(fill="plum4") + 
+  labs(title="Young Adult Observations from Each Year",x="Year",y="Count") +
+  theme_light()
+
+ggsave(yearcount,filename="figures/1_yearcount.png")
+
+#sex count
+sexcount <-YA_19_21 %>%
+  ggplot(aes(x=irsex)) + 
+  geom_bar(fill="plum4") + 
+  labs(title="Young Adult Observations by Sex",x="Sex",y="Count") +
+  theme_light()
+
+ggsave(sexcount,filename="figures/1_sexcount.png")
+
+#income count
+incomecount <- YA_19_21 %>%
+  ggplot(aes(income)) + 
+  geom_bar(fill="plum4") + 
+  labs(title="Young Adult Observations by Income",x="Income",y="Count") +
+  theme_light()
+
+ggsave(incomecount,filename="figures/1_incomecount.png")
+
+#alcohol frequency month
+alcmonth <- YA_19_21 %>%
+  ggplot(aes(iralcfm)) + 
+  geom_histogram(fill="plum4") + 
+  labs(title="Distribution of Monthly Alcohol Use Among Young Adults",x= "Times Drank in Past Month",y="Count") +
+  theme_light()
+
+ggsave(alcmonth,filename="figures/1_alcmonth.png")
+
+#marijuana frequency month
+mjmonth <-YA_19_21 %>%
+  ggplot(aes(irmjfm)) + 
+  geom_histogram(fill="plum4") + 
+  labs(title="Distribution of Monthly Marijuana Use Among Young Adults",x= "Times Used in Past Month",y="Count") +
+  theme_light()
+
+ggsave(mjmonth,filename="figures/1_mjmonth.png")
